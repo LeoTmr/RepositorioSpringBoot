fbb5f4d0fa954aa1f98ad60bff9c9b5d70562ed9
LeoTmr <leonardotrotta32@gmail.com>
2021-08-12 11:38:20 -0300

    Blog pessoal

--- blogPessoal/src/test/java/br/org/generation/blogPessoal/blogPessoalApplicationTests.java
+++ blogPessoal/src/test/java/br/org/generation/blogPessoal/blogPessoalApplicationTests.java
@@ -1,10 +1,10 @@
-package org.generation.blogPessoal;
+package br.org.generation.blogPessoal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BlogPessoalApplicationTests {
+class BlogpessoalApplicationTests {
 
 	@Test
 	void contextLoads() {
